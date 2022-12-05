fa1ef8ee9965f5d918d0103450c9b01db171d9f2
Alexander Lindhe <allindhe@gmail.com>
2022-12-05 22:12:19 +0100

    Another day, another test

--- app/manage.py
+++ app/manage.py
@@ -2,15 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from pathlib import Path
-
-
-# If file exist
-try:
-    with open(Path(__file__).with_name('django_key.txt'), 'r') as f:
-        os.environ['DJANGO_SECRET_KEY'] = f.read().strip()
-except FileNotFoundError:
-    pass
 
 
 def main():
